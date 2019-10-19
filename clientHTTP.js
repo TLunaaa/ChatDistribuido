b8a7a48d374e4275bd105a8760324accf9b92257
Nicolas Escude <nicolasescude.soft@gmail.com>
2019-10-19 20:14:56 -0300

    Heartbeat beating

--- clientHTTP.js
+++ clientHTTP.js
@@ -60,6 +60,7 @@ function send(){
 };
 
 options.path = '/register?username='+username+'&ip='+MHOST+'&port='+MPORT;
+send().end();
 heart.createEvent(1,(count,last)=>{
     send().end();
 })
@@ -74,6 +75,13 @@ server.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
 
 });
-server.bind(PORT, HOST);
+server.bind(MPORT, MHOST);
+//UDP-P2P-Sender
+var message = new Buffer('My KungFu is Good!');
 
-//UDP-P2P-
+var client = dgram.createSocket('udp4');
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + HOST +':'+ PORT);
+    client.close();
+});
